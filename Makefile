afa995562990beebf53bbd69999597975e0154c9
Marwan Moussa <marwan@mmoussa.23-2223>
2024-11-12 15:24:15 -0500

    First commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+exe: hamming_calculator.c
+	gcc -Wall -Werror -Wextra -o exe hamming_calculator.c
+
---
+++ hamming_calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+//for each bit in 1, check if it matches 2, if yes then ++, if one is longer than the other return lengths not equal
+//could use xor
+// O(n) complexity
+	if(argc != 3){
+		fprintf(stderr, "Please have two values.");
+	}
+	int a = argv[1];
+	int b = argv[2];
+	while(a )
+}
+123 % 10 = 3
+123 / 10 = 12
+
+111
+101
+
+111 % 10 = 1
+1011 % 10 = 1
+1 = 1
+111 / 10 = 11
