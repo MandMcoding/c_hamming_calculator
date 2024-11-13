5a2aa0d6e1cd6ad5db03b4245f6d15e78c7396d7
MandMcoding <marwanmoussacoding@gmail.com>
2024-11-12 22:52:43 -0500

    First Attempt

--- hamming_calculator.c
+++ hamming_calculator.c
@@ -7,10 +7,23 @@ int main(int argc, char** argv){
 	if(argc != 3){
 		fprintf(stderr, "Please have two values.");
 	}
-	int a = argv[1];
-	int b = argv[2];
-	while(a )
+	int a = *argv[1];
+	int b = *argv[2];
+	
+	int count = 0;
+	while((a % 10 != a % 100) && (b % 10 != b % 100)){
+		// count += a % 10 != b % 10;
+		if(a % 10 != b % 10){
+			count++;
+		}
+		a / 10;
+		b / 10;
+	}
+	if(a != b) count++;
+
+	return 0;
 }
+/*
 123 % 10 = 3
 123 / 10 = 12
 
@@ -21,3 +34,4 @@ int main(int argc, char** argv){
 1011 % 10 = 1
 1 = 1
 111 / 10 = 11
+*/
